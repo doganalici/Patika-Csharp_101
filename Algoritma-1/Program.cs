1c1c9f64ec4be8e8b53fcaf8df71bb3d12bf3547
Dogan Alici <doganalici2144@gmail.com>
2024-01-22 10:50:43 +0300

    Algoritma Örnek

---
+++ Algoritma-1/Program.cs
@@ -0,0 +1,26 @@
+﻿//Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n).
+//Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin.
+//Kullanıcının girmiş olduğu sayılardan çift olanlar console'a yazdırın.
+
+Console.Write("Hesaplamak istediğiniz sayı adedini giriniz : ");
+int adet=Convert.ToInt32(Console.ReadLine());
+
+int[] sayiDizisi= new int[adet];
+Console.WriteLine();
+for (int i = 0; i < adet; i++)
+{
+    Console.Write("{0}. Sayısı Giriniz: ", i + 1);
+    sayiDizisi[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("-------------");
+
+Console.Write("Girmiş Olduğunuz Çift Sayı/Sayılar: ");
+
+for (int i = 0; i < adet; i++)
+{
+    if (sayiDizisi[i] % 2 == 0)
+    {
+        Console.Write(sayiDizisi[i] + " ");
+    }
+}
---
+++ Algoritma-2/Program.cs
@@ -0,0 +1,33 @@
+﻿//Bir konsol uygulamasında kullanıcıdan pozitif iki sayı girmesini isteyin (n, m).
+//Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin.
+//Kullanıcının girmiş olduğu sayılardan m'e eşit yada tam bölünenleri console'a yazdırın.
+
+Console.Write("Kök sayıyı giriniz : ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+Console.Write("Hesaplamak istediğiniz sayı adedini giriniz : ");
+int adet = Convert.ToInt32(Console.ReadLine());
+int[] sayiDizisi = new int[adet];
+
+for (int i = 0; i < adet; i++)
+{
+    Console.Write("{0}. Sayısı Giriniz: ", i + 1);
+    sayiDizisi[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("-------------");
+
+
+Console.Write("Kök sayıya eşit yada tam bölünen sayılar ");
+
+for (int i = 0; i < adet; i++)
+{
+    if (m % sayiDizisi[i] == 0 || m == sayiDizisi[i])
+    {
+        Console.Write(sayiDizisi[i] + " ");
+    }
+}
+
+
